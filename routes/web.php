2aa82d197a079cd7c4f85a4fdb1bc09cc9d4e090
charlie <charlie.nguyen.goldenowl@gmail.com>
2023-06-24 15:19:06 +0700

    update default route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('register');
 });
 
 Route::view('/register','register')->name('register');
